f0484986074c03a01b5300e01138be2ee1087725
Anton Petrov <petrov.aap@gmail.com>
2015-05-02 13:51:46 +0300

    Added logger and other improvements

--- middleware/currentUser.js
+++ middleware/currentUser.js
@@ -1,4 +1,5 @@
 var errors = require('../errors');
+var logger = require('../utils/logger');
 
 module.exports = function(req, res, next) {
     res.locals = {};
---
+++ routes/auth/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* Auth route */
+
+router.post('/login', function(req, res, next) {
+
+});
+
+router.post('/logout', function(req, res, next) {
+
+});
+
+router.post('/singup', function(req, res, next) {
+
+});
+
+
+module.exports = router;
